3ac7070ca08d6afa9fa9b9342c1c2bc3aff90614
maikec@126.com <a6259272>
2019-07-16 14:06:02 +0800

    重复注解

---
+++ src/test/java/com/example/springboot/demo/annotation/RepeatAnnotation.java
@@ -0,0 +1,22 @@
+package com.example.springboot.demo.annotation;/**
+ * 广州奥凯信息咨询有限公司
+ * <p>
+ * 本源代码由《springboot》及其作者共同所有，未经版权持有者的事先书面授权，
+ * 不得使用、复制、修改、合并、发布、分发和/或销售本源代码的副本。
+ *
+ * @copyright Copyright (c) 2019-2022. （广州奥凯信息咨询有限公司）all rights reserved.
+ */
+
+import java.lang.annotation.*;
+
+/**
+ * 说明
+ *  @author maikec
+ *  @date 2019/7/10
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(RepeatAnnotations.class)
+public @interface RepeatAnnotation {
+    String value() default "";
+}
---
+++ src/test/java/com/example/springboot/demo/annotation/RepeatTest.java
@@ -0,0 +1,48 @@
+package com.example.springboot.demo.annotation;/**
+ * 广州奥凯信息咨询有限公司
+ * <p>
+ * 本源代码由《springboot》及其作者共同所有，未经版权持有者的事先书面授权，
+ * 不得使用、复制、修改、合并、发布、分发和/或销售本源代码的副本。
+ *
+ * @copyright Copyright (c) 2019-2022. （广州奥凯信息咨询有限公司）all rights reserved.
+ */
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * 说明
+ *  @author maikec
+ *  @date 2019/7/10
+ */
+public class RepeatTest {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> aClass = Class.forName(RepeatTest.class.getName());
+        Arrays.asList(aClass.getMethods()).forEach(method -> {
+            if (method.getName().startsWith("t")){
+                System.out.println(method.getName());
+                Arrays.asList(method.getAnnotations()).forEach(annotation -> {
+                    if (annotation instanceof RepeatAnnotations){
+                        Arrays.asList(((RepeatAnnotations) annotation).value()).forEach(repeatAnnotation -> {
+                            System.out.println(repeatAnnotation.value());
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    @RepeatAnnotation("1")
+    @RepeatAnnotation("2")
+    public void t(){
+
+    }
+    @RepeatAnnotations({
+            @RepeatAnnotation("11"),
+            @RepeatAnnotation("12")
+    })
+    public void t2(){
+
+    }
+
+}
